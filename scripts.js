0beb05d0950aaf759e3a26865133edab8d99c1da
Imran <imranradziii@gmail.com>
2022-08-07 02:25:59 +0100

    added pawns to the board, added some functions for prototyping to scripts.js

--- scripts.js
+++ scripts.js
@@ -7,9 +7,22 @@ const gameFlow = (() => {
 })();
 
 const player = (name, color) => {
+  const getName = () => {
+    return name;
+  }
+  const getColor = () => {
+    return color;
+  }
+  return {getName, getColor};
+};
 
-}
+const pawn = (color) => {
+  const getColor = () => {
+    return color;
+  }
+  return {getColor};
+};
 
 const gameBoard = (() => {
   
-})
+})();
