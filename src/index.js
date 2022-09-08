4c28e722684b15add66679713d733fca75612f33
Imran <imranradziii@gmail.com>
2022-09-08 07:24:20 +0100

    clean up unnecessary code

--- src/index.js
+++ src/index.js
@@ -1,15 +1,5 @@
 import './styles.css';
 import { gameBoard } from './modules/boardPieces';
 
-const player = (name, color) => {
-  const getName = () => {
-    return name;
-  }
-  const getColor = () => {
-    return color;
-  }
-  return { getName, getColor };
-};
-
 gameBoard.displayPawns();
 
