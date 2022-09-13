d88e674a25cbd46f3623ae63368baece2cef487e
Imran <imranradziii@gmail.com>
2022-09-13 11:30:23 +0100

    reconfigure eslint, fixed most of the styling errors in code flagged by eslint

--- src/modules/pawns.js
+++ src/modules/pawns.js
@@ -1,28 +1,16 @@
-import { gameFlow } from "./gameFlow";
+import { gameFlow } from './gameFlow';
 
 export const pawn = (name, color, row, column) => {
   const pawnName = name;
   const pawnColor = color;
+  const pawnImg = document.createElement('img');
   let pawnRow = row;
   let pawnColumn = column;
 
-  const getName = () => {
-    return pawnName;
-  }
-
-  const getColor = () => {
-    return pawnColor;
-  }
-
-  const getRow = () => {
-    return pawnRow;
-  }
-
-  const getColumn = () => {
-    return pawnColumn;
-  }
-  
-  const pawnImg = document.createElement('img');
+  const getName = () => pawnName;
+  const getColor = () => pawnColor;
+  const getRow = () => pawnRow;
+  const getColumn = () => pawnColumn;
 
   if (color === 'black') {
     pawnImg.setAttribute('src', 'svg/bpawn.svg');
@@ -37,32 +25,32 @@ export const pawn = (name, color, row, column) => {
   const pawnMove = (newRow, newColumn) => {
     pawnRow = newRow;
     pawnColumn = newColumn;
-  }
+  };
 
   const calculateLegalMoves = () => {
-    let pawnRowInt = parseInt(pawnRow);
+    const pawnRowInt = parseInt(pawnRow, 10);
     let forwardRow = '';
 
     if (
-      getColor() === 'black' && pawnRow === '1'
-      || getColor() === 'white' && pawnRow === '3') {
+      (getColor() === 'black' && pawnRow === '1')
+      || (getColor() === 'white' && pawnRow === '3')) {
       return [];
     }
 
-    if (getColor() === 'black' & pawnRow !== '1') {
+    if (getColor() === 'black' && pawnRow !== '1') {
       forwardRow = pawnRowInt - 1;
-    } else if (getColor() === 'white' & pawnRow !== '3') {
+    } else if (getColor() === 'white' && pawnRow !== '3') {
       forwardRow = pawnRowInt + 1;
     }
 
     const calculateForwardMove = () => {
-      let forwardColumn = [];
+      const forwardColumn = [];
       if (gameFlow.getPawnImg(forwardRow, getColumn(), getColor())
         === 'no') {
-          forwardColumn.push(`${forwardRow.toString()}${pawnColumn}`)
-        }
+        forwardColumn.push(`${forwardRow.toString()}${pawnColumn}`);
+      }
       return forwardColumn;
-    }
+    };
 
     const calculateDiagonalMove = () => {
       let diagonalColumn = [];
@@ -70,72 +58,68 @@ export const pawn = (name, color, row, column) => {
         pawnColumn === 'a' || pawnColumn === 'c'
       ) {
         if (
-        gameFlow.getPawnImg(forwardRow, 'b', getColor())
+          gameFlow.getPawnImg(forwardRow, 'b', getColor())
         !== 'no'
         && gameFlow.getPawnImg(forwardRow, 'b', getColor())[0]
         !== getColor()[0]) {
           diagonalColumn = ['b'];
         }
       } else if (pawnColumn === 'b') {
-        let validColumn = ['a', 'c'];
-        for (const col of validColumn) {
+        const validColumn = ['a', 'c'];
+        validColumn.forEach((col) => {
           if (
-          gameFlow.getPawnImg(forwardRow, col, getColor())
-          !== 'no'
-          && gameFlow.getPawnImg(forwardRow, col, getColor())[0]
-          !== getColor()[0]) {
+            gameFlow.getPawnImg(forwardRow, col, getColor())
+            !== 'no'
+            && gameFlow.getPawnImg(forwardRow, col, getColor())[0]
+            !== getColor()[0]) {
             diagonalColumn.push(col);
           }
-        }
+        });
       }
 
-      let moveableTile = [];
-      for (let i of diagonalColumn) {
+      const moveableTile = [];
+      diagonalColumn.forEach((i) => {
         moveableTile.push(`${forwardRow.toString()}${i}`);
-      }
+      });
       return moveableTile;
-    }
-    
-    return calculateForwardMove()
-            .concat(calculateDiagonalMove());
-  }
+    };
 
-  return { getName,
-           getColor,
-           getRow,
-           getColumn,
-           pawnImg,
-           pawnMove,
-           calculateLegalMoves };
-}
+    return calculateForwardMove()
+      .concat(calculateDiagonalMove());
+  };
+
+  return {
+    getName,
+    getColor,
+    getRow,
+    getColumn,
+    pawnImg,
+    pawnMove,
+    calculateLegalMoves,
+  };
+};
 
 export const pawns = (() => {
   const abc = ['a', 'b', 'c'];
-  let list = {};
+  const list = {};
 
   for (let i = 1; i < 4; i++) {
-    list[`bpawn${i}`] = pawn(`bpawn${i}`, 'black',
-    '3', abc[i - 1]);
-    list[`wpawn${i}`] = pawn(`wpawn${i}`, 'white',
-    '1', abc[i - 1]);
+    list[`bpawn${i}`] = pawn(`bpawn${i}`, 'black', '3', abc[i - 1]);
+    list[`wpawn${i}`] = pawn(`wpawn${i}`, 'white', '1', abc[i - 1]);
   }
 
-  const getList = () => {
-    return list;
-  }
+  const getList = () => list;
 
   const removePawn = (pawnName) => {
     delete list[pawnName];
-  }
+  };
 
   const resetPawns = () => {
     for (let i = 1; i < 4; i++) {
-      list[`bpawn${i}`] = pawn(`bpawn${i}`, 'black',
-      '3', abc[i - 1]);
-      list[`wpawn${i}`] = pawn(`wpawn${i}`, 'white',
-      '1', abc[i - 1]);
+      list[`bpawn${i}`] = pawn(`bpawn${i}`, 'black', '3', abc[i - 1]);
+      list[`wpawn${i}`] = pawn(`wpawn${i}`, 'white', '1', abc[i - 1]);
     }
-  }
+  };
 
   return { getList, removePawn, resetPawns };
 })();
